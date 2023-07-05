f015782b634bffb927d11a07cd76b0f56869e330
ClemRoy <clement.roy1@gmail.com>
2023-07-05 17:23:34 +0200

    added heroku deployment to workflow

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,10 +10,16 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
-ALLOWED_HOSTS = ["orangecountylettings-db36e009af6c.herokuapp.com/"]
+# SECURITY WARNING: don't run with debug turned on in production!
+if not IS_HEROKU_APP:
+    DEBUG = True
+
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
